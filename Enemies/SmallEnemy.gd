993688a235d24ef79288f67fafb6e15bd4527d27
connor <connor@pop-os.localdomain>
2020-07-11 13:18:34 -0400

    added comments to small enemyAI code

--- Enemies/SmallEnemy.gd
+++ Enemies/SmallEnemy.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-
 onready var player = get_parent().get_node("Hero")
 var speed = 175
 var velocity = Vector2()
@@ -9,9 +8,10 @@ var velocity = Vector2()
 func _ready():
 	pass
 
-
 func _process(delta):
+	#if the player is in line of sight
 	if(is_in_line_of_sight(player)):
+		#add the direction of the player to enemy velocity
 		if player.position.x > position.x:
 			velocity.x += speed
 		if player.position.x < position.x:
@@ -21,14 +21,17 @@ func _process(delta):
 		if player.position.y < position.y:
 			velocity.y -= speed
 	else:
+		#if can't see player, don't move
 		velocity.x = 0
 		velocity.y = 0
 
 func _physics_process(delta):
-	#velocity = move_and_slide(velocity * delta)
+	#normalizes the velocity vector, and then sets it to the declared speed
 	velocity = velocity.normalized() * speed
 	move_and_slide(velocity)
 
+#checks to see if our enemy has line of sight with the player
+#Pulled this from the internet, not entirely sure how it works lol
 func is_in_line_of_sight(thing):
 	var space = get_world_2d().direct_space_state
 	if thing != null:
