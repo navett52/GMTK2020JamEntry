c23a0ea3a6e33a24dd2b4b01124a8d9ec599631a
connor <connor@pop-os.localdomain>
2020-07-11 16:49:03 -0400

    added aoe attack to enemy

--- Enemies/BigEnemy.gd
+++ Enemies/BigEnemy.gd
@@ -6,7 +6,7 @@ export var dash_speed = 500
 var velocity = Vector2()
 var d_velocity = Vector2()
 var states = ["dash", "AOE", "shoot"]
-var state = "dash"
+var state = "AOE"
 var waiting = false
 var dashing = false
 var timer = 0
@@ -16,7 +16,7 @@ func _ready():
 	pass
 
 func _process(delta):
-	print(distance_to_object(player))
+	#print(distance_to_object(player))
 	#if the player is in line of sight
 	if(is_in_line_of_sight(player) && distance_to_object(player) <= 400):
 		
@@ -37,13 +37,16 @@ func _physics_process(delta):
 	if(state == "dash" && distance_to_object(player) <= 200 && !waiting):
 		d_velocity = (player.get_position()-self.get_position()).normalized() * dash_speed
 		waiting = true
-		wait(3)
+		waitDash(3)
+	elif(state == "AOE" && distance_to_object(player) <= 100 && !waiting):
+		waiting = true
+		waitAOE(2)
 	elif(!waiting):
 		#normalizes the velocity vector, and then sets it to the declared speed
 		velocity = velocity.normalized() * speed
 		move_and_slide(velocity)
 	elif(waiting && dashing):
-		if(timer < 20):
+		if(timer < 30):
 			timer += 1
 			move_and_slide(d_velocity)
 		else:
@@ -55,7 +58,19 @@ func dash():
 	dashing = true
 
 func AOE():
+	showSplo()
+	get_tree().create_timer(0.5).connect("timeout", self, "hideSplo")
+	for i in get_node("Area2D").get_overlapping_bodies():
+		if(i.get_name() == "Hero"):
+			print("sploded")
 	pass
+
+func showSplo():
+	get_node("splosion").visible = true
+
+func hideSplo():
+	get_node("splosion").visible = false
+	waiting = false
 
 func shoot():
 	pass
@@ -64,8 +79,12 @@ func distance_to_object(thing):
 	if thing != null: 
 		#return sqrt(abs((int(self.get_position().x - thing.get_position().x)^2) + (int(self.get_position().y - thing.get_position().y)^2)))
 		return self.get_global_transform().get_origin().distance_to(thing.get_global_transform().get_origin())
-func wait(time):
+
+func waitDash(time):
 	get_tree().create_timer(time).connect("timeout", self, "dash")
+
+func waitAOE(time):
+	get_tree().create_timer(time).connect("timeout", self, "AOE")
 
 #checks to see if our enemy has line of sight with the player
 #Pulled this from the internet, not entirely sure how it works lol
