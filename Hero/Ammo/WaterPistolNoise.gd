32534a12df951c168d441c2d7b8dbd8fa1213541
Evan Tellep <evantellep@gmail.com>
2020-07-11 16:48:30 -0400

    Added all guns and sounds

---
+++ Hero/Ammo/WaterPistolNoise.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer2D
+
+
+# Sounds
+var sound1 = load("res://Hero/Ammo/Sounds/S_water pistol _1.wav")
+var sound2 = load("res://Hero/Ammo/Sounds/S_water pistol _2.wav")
+
+var rand = RandomNumberGenerator.new()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	if (randf() < 0.5):
+		stream = sound1
+	else:
+		stream = sound2
--- Hero/Gun.gd
+++ Hero/Gun.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var direction = Vector2()
 var Shotgun = load("res://Hero/Ammo/Shotgun.tscn")
+var WaterPistol = load("res://Hero/Ammo/WaterPistol.tscn")
+var IceRifle = load("res://Hero/Ammo/IceRifle.tscn")
+var FlameRifle = load("res://Hero/Ammo/FlameRifle.tscn")
+var BigAssExplosion = load("res://Hero/Ammo/BigAssExplosion.tscn")
 
 
 func _process(delta):
@@ -13,7 +17,11 @@ func _process(delta):
 # Needs some work still to randomly assign the different types of ammo
 func shoot():
 	var mouse_direction = (get_global_mouse_position() - self.global_position).normalized()
-	var bullet = Shotgun.instance()
+	# var bullet = Shotgun.instance()
+	# var bullet = WaterPistol.instance()
+	# var bullet = IceRifle.instance()
+	# var bullet = FlameRifle.instance()
+	var bullet = BigAssExplosion.instance()
 	bullet.global_position = global_position
 	# Below is some weird math I had to do to get the bullets to spawn in the right direction
 	bullet.rotation = -atan2(mouse_direction.x, mouse_direction.y) + (PI/2)
